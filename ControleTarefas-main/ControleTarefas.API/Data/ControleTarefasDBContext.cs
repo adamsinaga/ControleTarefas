40853774a10cc10f068bbdb9833a43360f92f34e
adamsinaga <adamsinaga@hotmail.com>
2023-05-22 20:49:41 -0400

    update

---
+++ ControleTarefas-main/ControleTarefas.API/Data/ControleTarefasDBContext.cs
@@ -0,0 +1,20 @@
+﻿using ControleTarefas.API.Data.Map;
+using ControleTarefas.API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ControleTarefas.API.Data
+{
+    public class ControleTarefasDBContext : DbContext
+    {
+        public ControleTarefasDBContext(DbContextOptions<ControleTarefasDBContext> options) : base(options) { 
+        
+        }
+        public DbSet<Usuario> Usuarios { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfiguration(new UsuarioMap());
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
---
+++ ControleTarefas-main/ControleTarefas.API/Models/Tarefa.cs
@@ -0,0 +1,12 @@
+﻿namespace ControleTarefas.API.Models
+{
+    public class Tarefa
+    {
+            public int Id { get; set; }
+            public string? Nome { get; set; }
+            public string? Descricao { get; set; }
+            public StatusTarefa Status { get; set; }
+            public int? UsuarioId { get; set; }
+            public virtual Usuario? Usuario { get; set; }   
+    }
+}
